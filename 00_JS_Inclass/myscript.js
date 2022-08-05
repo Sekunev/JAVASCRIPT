8440875e3a3437dd9efcabae064528f506d54bc0
sekune <ahlatlipost@gmail.com>
2022-08-06 01:10:51 +0300

    05.08.22

--- 00_JS_Inclass/myscript.js
+++ 00_JS_Inclass/myscript.js
@@ -4,3 +4,18 @@
 
 // let yetişkinmi= confirm("Yetişkinmisin?");
 // alert(yetişkinmi); // true if OK is pressed
+
+
+var a = b = c = 5;
+console.log('a =',a, 'b =',b, 'c =',c); 
+// a = 5 b = 5 c = 5
+
+var firstName = 'John' , lastName = 'Doe';
+console.log(firstName,lastName); 
+// John Do
+
+// var a = 4; // 4
+// var a = 2; // 2
+
+// let a = 5;
+// let a = 3; // error
