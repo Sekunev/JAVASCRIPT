0252f4f9f1ac26f66e8784a6e19468995779ca3c
sekune <ahlatlipost@gmail.com>
2022-08-05 19:05:26 +0300

    05.08.22

---
+++ 00_JS_Inclass/myscript.js
@@ -0,0 +1,6 @@
+// let age = prompt('kaç yaşındasın', 25)
+// alert( `Sen ${age} yaşındasın` )
+// //  Metin mesajı, ziyaretçi için bir giriş alanı ile Tamam/İptal düğmeleri içeren kalıcı bir pencere.
+
+// let yetişkinmi= confirm("Yetişkinmisin?");
+// alert(yetişkinmi); // true if OK is pressed
