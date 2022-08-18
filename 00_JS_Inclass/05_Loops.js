d039e6850060b65489b8df4632273f3735fd852d
sekune <ahlatlipost@gmail.com>
2022-08-18 13:30:59 +0300

    18.08.22

--- 00_JS_Inclass/05_Loops.js
+++ 00_JS_Inclass/05_Loops.js
@@ -16,26 +16,64 @@
 //! for (initializer; condition; iterator) {
    // for loop body
 // }
-// örnek:
 
+//? Ornek: 1 den n kadar sayiları toplayan kodu yaziniz.
+
+// const n = Number(prompt("Bir sayi girniz:"));
 // let sum = 0;
 
-// for (let i = 0; i <= 5; i++) {
-//     console.log(i);
+// for (let i = 0; i <= n; i++) {
 //     sum += i
+//     console.log(sum, i);
 // }
-// console.log(sum);
+// console.log('Sonuç: ', sum);
+
+//? Ornek: 0-100 Arasinda 10 adet rasgele tamsayi sayi ureten kodu
+//? for dongusu ile yaziniz..
+
+// for (let i = 1; i <= 10; i++) {
+//   const rasgele = Math.round(Math.random() * 100);
+//   console.log(rasgele);
+// }
+
+//* NOT:
+//* Math.floor() =>en yakin alt tamsayiya yuvarlar
+//* Math.ceil() =>en yaki ust tamsayiya yuvarlar
+//* Math.round() => degerine gore yuvarlar.
+//* Math.trunc() => kesirli sayinin tam kismini alir.
+
+//? ÖRNEK: Girilen bir sayinin Asal olup olmadigini yazdiran kodu
+//? for dongulerini kullanarak yaziniz.
+
+// const sayi = Number(prompt("Pozitif Bir sayi giriniz:"));
+// let asal = true;
+
+// if (sayi <= 1) {
+//   alert("Sayi 1'den buyuk olmalidir.");
+// } else {
+//   for (let i = 2; i < sayi; i++) {
+//     if (sayi % i === 0) {
+//       asal = false;
+//       break;
+//     }
+//   }
+
+//   const sonuc = asal === true ? "ASALDIR" : "ASAL DEGILDIR";
+//   console.log(`${sayi} ${sonuc}`);
+// }
+
 
 //? işlem sırası
 // 1. Başlatıcı(initializer) , değişkenleri başlatır ve/veya bildirir ve yalnızca bir kez yürütülür.
 // 2. koşul değerlendirilir. True ise kod bloğu çalışır false ise çalışmaz.
 // 3. 3. parametredeki yineleyici başlatıcının değerini günceller. 
 //! dikkat Yineleyici , true koşul olduğunda başlatıcının değerini günceller.
+// ! başlatıcıda const kullanılmaz çünkü değer değişecek.
 
 // örnek:
 
-let sayılar = [1, 3, 5, 9]
-let toplam = 0
+// let sayılar = [1, 3, 5, 9]
+// let toplam = 0
 
 // for(let i = 0; i < sayılar.length; i++) {
 //     toplam += sayılar[i];
@@ -72,6 +110,17 @@ let toplam = 0
 // }
 // console.log(val)
 
+//?  *** borsa programı ***
+
+// let prices = [7,6,4,3,1]
+// let profit = 0;
+// for (let index = 1; index < prices.length; index++) {
+//     if (prices[index] > prices[index-1]) {
+//         profit += prices[index] - prices[index-1]
+//     }
+// }
+
+
 //! ********* while *********
 
 //? The syntax of the while loop is:
@@ -87,7 +136,21 @@ let toplam = 0
 // }
 // Aslında for döngüsündeki parametreler aynen kullanıldı. ama parantez içerisinde değil. 
 
-//! ********* do while *********
+//? ORNEK: Kullanicidan 0-100 arasinda bir not isteyen ve girilen not
+//? 0-100'den farkli ise Kullaciniyi uyararak yeniden 0-100 arasinda
+//? not girmeye zorlayan kodu while dongusu ile yaziniz.
+
+// let not = +prompt("Lutfen 0-100 arasinda bir not girniz.");
+
+// while (not < 0 || not > 100) {
+//   console.log("Not 0-100 arasinda olmalidir");
+//   not = prompt("Lutfen 0-100 arasinda bir not giriniz.");
+// }
+
+// console.log("Giridiginiz Not:", not);
+
+
+//! ********* DO-WHILE *********
 
 // Bu, koşul false olsa bile döngünün her zaman en az bir kez yürütüleceği anlamına gelir.
 // //? do...while Döngünün sözdizimi şöyledir:
@@ -110,6 +173,47 @@ let toplam = 0
 //     number = parseInt(prompt('bir numara gir'))
 // } while ( number >= 0)
 // console.log(`toplam ${sum}`);
+
+//? ÖRNEK: 10 kere konsola Merhaba yazdıran uygulamayı do-while döngüsü ile yazınız.
+
+// let i = 1
+// do {
+//     console.log('merhaba\n')
+//     i++
+// }while(i <= 10);
+
+
+
+//? ÖDEV: Klavyeden Q veya q karakteri girilene kadar not girişi yapan ve bu  karakterlerden birisi girildiğinde O ana kadar girilen tüm notların ortalamasını hesaplayarak konsola ortalamayı bastıran uygulamayı yazınız.
+
+// let not;
+// let notTop = 0;
+// let sayac = 0
+// do {
+//     not = prompt('Not giriniz veya çıkmak için Q tuşuna basınız.');
+//     if (not == "Q" || not == "q") {break;}
+//     notTop += +not
+//     ++sayac
+// }while(not != "Q" && not != "q")
+// console.log(`not ortalaması: ${notTop/sayac}`);
+
+
+
+//? ORNEK: Kullanicidan 0-100 arasinda bir not isteyen ve girilen not
+//? 0-100'den farkli ise Kullaciniyi uyararak yeniden 0-100 arasinda
+//? not girmeye zorlayan kodu do-while dongusu ile yaziniz.
+
+// let not1;
+// do {
+//   not1 = +prompt("Lutfen 0-100 arasinda bir not girniz.");
+//   if (not1 < 0 || not1 > 100) {
+//     console.log("Not 0-100 arasinda olmaldir.");
+//     alert("Not 0-100 arasinda olmaldir.");
+//   }
+// } while (not1 < 0 || not1 > 100);
+
+// console.log("Giridiginiz Not:", not1);
+
 
 //! ********* break Statement *********
 
@@ -161,3 +265,39 @@ let toplam = 0
 // var sum = sum + i; i++;
 // }
 // console.log ("The total is: " + sum);
+
+
+// ödev 
+//* Program 0*100 arasında rasgele bir sayı tutacak ve kullanıcının bu sayıyı 5 kere(hak) de bilmesini isteyecektir. Her yanlışta hakkını bir düşürecek ve ARTTIR/AZALT diyerek kullanıcıyı yönlendirecektir.Sonuç olarak kullanıcının hakkı 0 olursa "Üzgünüz bilemediniz" eğer bildi ise "Tebrikler ... denemede bildiniz" yazacaktır.
+
+// let hak = 5;
+// const rastgele = Math.round(Math.random() * 100);
+// console.log(rastgele);
+// let tahmin;
+// let devammı = true
+
+// while (devammı) {
+//     do {
+//         tahmin = Number(prompt("Lutfen 0-100 arasinda bir tahmin giriniz:"));
+//         hak -= 1;
+//         if (tahmin === rastgele) {
+//           console.log(`Tebrikler ${5 - hak} kerede bildiniz.`);
+//           break;
+//         } else if (tahmin < rastgele) {
+//           console.log("ARTTIR ⬆️");
+//         } else {
+//           console.log("AZALT ⬇️");
+//         }
+//     } while (hak > 0);
+      
+//     if (tahmin !== rastgele) {
+//     console.log("Uzgunuz oyunu kaybettiniz 😔😔");
+//     }
+
+//     let bitsinmi = prompt("Tekrar oynamak istermisini E/H?")
+//     if ( bitsinmi === "E" || bitsinmi === "e") {
+//         devammı = true;
+//         hak = 5;
+//     }else{devammı = false}
+
+// }
