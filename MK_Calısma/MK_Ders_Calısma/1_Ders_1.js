027208bbf1bc6b4474c77af27bb01380094720df
Mustafa_Kayıkcı <mktr6363@gmail.com>
2022-08-24 22:39:28 +0200

    dosyalar duzenlendı

--- MK_Calısma/MK_Ders_Calısma/1_Ders_1.js
+++
@@ -1,5 +0,0 @@
-const prompt = require("prompt-sync")();
-// bu komutu asagıda termınalı calıstırmak ıcın yuklemek zorundayız. aksı takdırde promt calısmayacak hata verecektır. bu arada node_modules folder ını da genel klasorumuzun ıcıne konumlandırdık.
-const a = Number(prompt("bır sayi giriniz: "));
-console.log(a);
-console.log(typeof a);
