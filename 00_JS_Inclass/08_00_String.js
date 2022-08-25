e703701bb85f6c197383ebe4513ad1f0f6421b62
sekune <ahlatlipost@gmail.com>
2022-08-25 12:46:18 +0300

    25.08.22

---
+++ 00_JS_Inclass/08_00_String.js
@@ -0,0 +1,262 @@
+// ?======================================================
+// ?                     STRING METOTLARI
+// ! KAYNAK: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+// ?======================================================
+
+console.log("*** STRING METHODS ***");
+
+//!1- Klasik yöntem (String Literals) ile string tanimlanirsa bu string
+//! bir primitive degiskendir.
+let str1 = "Clarusway";
+//Char => C, l, a,....
+const str2 = " Hello";
+const str3 = " Full Stack";
+
+console.log(str1, typeof str1);
+
+//!2 - Non-primitive String tanımlama
+const str4 = new String("Non-Primitive");
+console.log(typeof str4);
+
+//?----------------------------------------
+console.log(str1.toLowerCase());
+
+//* Normalde primitive veri tiplerinin property veya metodu olmaz.
+//* Ancak,Javascript otomatik olarak primitive String'ler için object wrapper oluşturur.
+//* Bu sayede, String nesnelerine ait olan fonksiyonlar veya özellikleri
+//* primitive string'ler ile kullanılabilir hale gelir.
+//* Sonrasında oluşan bu object bellekten otomatik olarak kaldırılır.
+
+//? Ek Açıklama için : https://javascript.info/primitives-methods
+
+const sayi = 5;
+const str5 = str1 + str2 + str3 + sayi; // Concatination
+console.log(str5, typeof str5);
+
+//* ---------------------------------------------------
+//*  concat() - immutable
+//* ---------------------------------------------------
+
+console.log(str1.concat(str2)); //* Orjinal degerler bozulmaz
+console.log(str1, str2);
+
+//? Assigning ile degiskenin degeri degistirilebilir.
+str1 = str1.concat(str3, " Path");
+console.log(str1);
+
+//* ----------------------------------------------------------
+//* toUpperCase(), toLowerCase() --immutable
+//* ----------------------------------------------------------
+
+const myName = "Noah Adams";
+console.log(myName.toUpperCase());
+console.log(myName.toLowerCase());
+
+let yourName = "ismet";
+// yourName = yourName.toUpperCase();
+// yourName = yourName.toLocaleUpperCase('tr');
+yourName = yourName.toLocaleUpperCase();
+console.log(yourName);
+
+//toLocaleUpperCase bir dizeyi küçük harflere dönüştürmek için geçerli yerel ayarı kullanır. TR karekterlerinde fark edilebilir.
+// parantez içine dil kodu yazma paramaetreside bulunuyor.
+
+//! ORNEK: iki string'i esit veya degil diye kiyaslayarak sonucu
+//! donduren fonksiyonu yaziniz.
+
+// const esitMi = (str1, str2) => {
+//   return str1.toLocaleUpperCase() === str2.toLocaleUpperCase()
+//     ? `${str1} ile ${str2} Esittir`
+//     : `${str1} ile ${str2} Esit degildir`;
+// };
+
+// süslü parantez açtığımız için return kullanmamız gerekiyor. ancak tek ifade olduğu için süslü açmaya gerek yok.  dolayısıyla retun'a da gerek yok. aşağıdaki gibi.
+
+const esitMi = (str1, str2) =>
+  str1.toLocaleUpperCase() === str2.toLocaleUpperCase()
+    ? `${str1} ile ${str2} Esittir`
+    : `${str1} ile ${str2} Esit degildir`;
+
+console.log(esitMi("MerHaba", "MERHABA"));
+console.log(esitMi("Hello", "Hell"));
+
+//* ----------------------------------------------------------
+//* localeCompare()
+//* ----------------------------------------------------------
+const a = "réservé"; // with accents, lowercase
+const b = "RESERVE"; // no accents, uppercase
+console.log(a.localeCompare(b, "en", { sensitivity: "base" }));
+
+//? 'BASE: a ≠ b, a = á, a = A'
+//? 'ACCENT: a ≠ b, a ≠ á, a = A'
+//? 'CASE: a ≠ b, a = á, a ≠ A'
+
+//* ----------------------------------------------------------
+//* charAt()
+//* ----------------------------------------------------------
+const str6 = "Full Stack Path and DS path";
+console.log(str6.charAt(5)); //* S
+console.log(str6.charAt(4)); //* " "
+console.log(str6.charAt()); //* F (ilk indistekini dondurur)
+console.log(str6.charAt(14)); //* h
+
+//! lenght bir property (objenin degiskenidir) dir ve string'in
+//! karakter sayisini tutar.
+console.log(str6.length); //* 27
+
+//* String son harfini ogrenmek istersek
+console.log(str6.charAt(str6.length - 1)); //* 0 -26 => 27 eleman
+
+//* ----------------------------------------------------------
+//* includes() - case sentive
+//* ----------------------------------------------------------
+const word = "To be or not to be, that is the question";
+
+console.log(word.includes("to be")); //? true
+console.log(word.includes("That")); //? false
+console.log(word.includes("")); //? true
+console.log(word.includes("to be", 14)); //? false
+console.log(word.includes("to be", 13)); //? true
+
+//! NOT: case insentive kullanmak icin str baslangicta kucuk veya
+//! buyuk harfe cevirilerek arama yapilabilir.
+//* ----------------------------------------------------------
+//* indexOf() , lastIndexOf() - case sensitive
+//* ----------------------------------------------------------
+
+const toBe = word.indexOf("or");
+console.log(toBe); //? 6
+console.log(word.indexOf("be")); //? 3
+console.log(word.lastIndexOf("be")); //? 16
+console.log(word.lastIndexOf("BE")); //? -1
+
+//* ----------------------------------------------------------
+//* search()
+//* ----------------------------------------------------------
+
+const buyukHarf = /[A-Za-z]/;
+const nokta = /[.]/;
+const virgul = /[,]/;
+console.log(word.search(buyukHarf)); //? ilk buyuk harfin indeksi 0
+console.log(word.search(virgul)); //?  18
+console.log(word.search(nokta)); //? -1 // Belirtilen değeri bulamayınca -1 döndürür.
+
+//* ----------------------------------------------------------
+//* startsWidth() , endsWith() - case sensitive
+//* ----------------------------------------------------------
+let word2 = "Sen gulunce guller acar gul pembe!";
+console.log(new String(word2));
+console.log(word2.startsWith("Sen")); //? true
+console.log(word2.startsWith("gulunce")); //? false
+console.log(word2.startsWith("gulunce", 4)); //? true
+console.log(word2.endsWith("!")); //? true
+console.log(word2.endsWith("acar", 23)); //? true
+
+//* ----------------------------------------------------------
+//*  replace(searchFor, replaceWith) --immutable
+//* ----------------------------------------------------------
+let oku = "Oku Baban gibi, saf olma, saf olma";
+// console.log(oku.replace("saf olma", "basarili ol"));
+oku = oku.replace("saf olma", "basarili ol");
+// oku = oku.replace(/saf olma/gi, "basarili ol");
+console.log(oku);
+
+oku = oku.replace(/BASARILI/gi, "Zengin");
+console.log(oku);
+
+//! NOT: replace metodu eger Regex ile aksi belirtilmiyorsa (g) sadece ilk buldugunu degistirir.
+
+//* ----------------------------------------------------------
+//*  replaceAll() --immutable
+//* ----------------------------------------------------------
+let degistir = "daglar daglar yol ver gecem..";
+degistir = degistir.replaceAll("daglar", "Dağlar");
+console.log(degistir);
+
+//?Regex de kullanilabilir.
+let degistir2 =
+  "No woman, no cry" +
+  " No woman, no cry, eh, yeah" +
+  " Little darling, don't shed no tears" +
+  " No woman, no cry, eh";
+
+console.log(degistir2.replaceAll(/no/gi, "Hayir"));
+
+//Büyük/küçük harf duyarlılığını değiştirmek için "i" kullan.
+
+//* ----------------------------------------------------------
+//*  slice(beginIndex[, endIndex])
+//*  substring(beginIndex[, endIndex])
+//*  substr (depreceated)
+//* ----------------------------------------------------------
+const veysel = "Uzun ince bir yoldayim yuruyorum gunduz gece..";
+
+const sliced = veysel.slice(33);
+console.log(sliced, typeof sliced); //? gunduz gece..
+
+console.log(veysel.slice(17, 30)); //? dayim yuruyor.
+console.log(veysel.slice(-10)); //? duz gece..
+console.log(veysel.slice(-23, -19)); //? yuru
+
+console.log(veysel.substring(17, 30)); //? dayim yuruyor
+//! negatif indeks substring ile kullanilamaz.
+console.log(veysel.substring(-10)); //? Uzun ince bir yoldayim yuruyorum gunduz gece..
+
+//* ----------------------------------------------------------
+//* split([sep [, limit] ])
+//* ----------------------------------------------------------
+const tarkan = "Gel gunduzle gece olalim";
+const splited = tarkan.split(" ");
+console.log(splited, typeof splited); //! Bosluklara göre ayirarak Array'e cevirdi.
+
+const chars = tarkan.split("");
+console.log(chars); //* null karakterine göre harfleri ayırarark bir char dizisi oluşturdu.
+
+//* ----------------------------------------------------------
+//* trim();
+//* ----------------------------------------------------------
+const ramazan = "    Hoş geldin ya şehri Ramazan     ";
+console.log(ramazan);
+console.log(ramazan.length); // 36
+console.log(ramazan.trim());
+console.log(ramazan.trim().length); //27
+
+//! ODEV1: tarkan string'indeki kelimelerin sayisini donduren
+//! bir fonksiyon yaziniz.
+
+let uzunluk = (dize) => dize.split(" ").length;
+
+console.log(uzunluk("uzun ince bir yoldayim gidiyorum gündüz gece."));
+
+//! ODEV2:
+//! ----------------------------------------------------------
+// https://www.youtube.com/watch?v=b7vfp5G4brE
+// https://youtu.be/b7vfp5G4brE
+
+//! //////////  ÖDEV :Youtube adres kısaltma   ///////
+let yazi = "https://www.youtube.com/watch?v=b7vfp5G4brE";
+let kısaHali = (yazi) => {
+  let parca1 = yazi.slice(0, 8);
+  let parca2 = yazi.slice(12, 17);
+  let parca3 = yazi[19];
+  let parca4 = yazi.slice(17, 19);
+  let parca5 = yazi[23];
+  let parca6 = yazi.slice(32);
+  return parca1 + parca2 + parca3 + parca4 + parca5 + parca6;
+};
+
+console.log(kısaHali(yazi));
+
+// alternatif çözüm:
+let yaziKısa1 = yazi.replace("be.com/watch?v=", ".be/");
+let yaziKısa2 = yaziKısa1.replace("www.", "");
+
+console.log(yaziKısa2);
+
+//! ORNEK: iki string'i esit veya degil diye kiyaslayarak sonucu
+//! donduren fonksiyonu yaziniz.
+
+let esitmi = (str1, str2) =>
+  str1.toLocaleUpperCase === str2.toLocaleUpperCase ? "eşit" : "eşit değil";
+
+console.log(esitmi("ASd", "asd"));
