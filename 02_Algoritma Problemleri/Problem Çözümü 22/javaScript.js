808e1060fdb8959a154c1ce2a009042e222cf280
sekune <ahlatlipost@gmail.com>
2022-10-20 17:13:04 +0300

    20.10.22

--- 02_Algoritma Problemleri/Problem Çözümü 22/javaScript.js
+++ 02_Algoritma Problemleri/Problem Çözümü 22/javaScript.js
@@ -4,7 +4,25 @@ let sayi = +prompt("Bir sayı giriniz");
 
 let sonuc = 1;
 
-for(let i = 1; i <= sayi; i++) {
-    sonuc *= i;  
+for (let i = 1; i <= sayi; i++) {
+  sonuc *= i;
 }
-console.log(sonuc)
+console.log(sonuc);
+
+// mülakat
+function initButtons() {
+  var body = document.body,
+    button,
+    i;
+
+  for (i = 0; i < 5; i++) {
+    button = document.createElement("button");
+    button.innerHTML = `Button ${i}`;
+    button.addEventListener("click", (e) => {
+      alert("i");
+    });
+    body.appendChild(button);
+  }
+}
+5;
+initButtons();
